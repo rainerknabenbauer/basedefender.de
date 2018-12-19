5f3dacb2b797c418acc7cc322bc4973ca39aae21
nykon <rainer.knabenbauer@posteo.de>
2018-12-19 15:09:21 +0100

    Added content blocks

--- cv.js
+++ cv.js
@@ -1,3 +1,41 @@
+
+// NAVIGATION
+
+function onClickNavigationLeft(div) {
+
+    displayContent(div);
+}
+
+function onClickNavigationMiddle(div) {
+    
+    displayContent(div);
+}
+
+function onClickNaviationLeft(div) {
+    
+    displayContent(div);
+}
+
+function displayContent(div) {
+
+    hideContent();
+
+    div.style.display = "block";
+    div.textContent = "klicked";
+}
+
+// CONTENT
+function hideContent(element) {
+
+    let elements = document.getElementsByClassName("content");
+
+    elements.array.forEach(element => {
+        element.style.display = "none";
+    });
+}
+
+
+
 function firstDiv(div) {
 
     div.textContent = "foobar";
