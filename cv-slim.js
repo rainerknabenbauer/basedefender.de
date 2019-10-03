f881600d859d567af95abaf564b191016d37ec3f
Rainer Knabenbauer <contact@basedefender.de>
2019-10-03 17:14:07 +0200

    First layout of professional page

--- cv-slim.js
+++ cv-slim.js
@@ -1,38 +1,38 @@
 
 let fadeInSpeed = "fast";
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     /** NAVIGATION */
 
-    $( "#header-latest" ).click(function() {
+    $("#header-latest").click(function () {
 
         let content = "content-latest";
 
         hideContent(content);
     });
 
-    $( "#header-professional" ).click(function() {
+    $("#header-professional").click(function () {
 
         let content = "content-professional";
 
         hideContent(content);
     });
 
-    $( "#header-forFun" ).click(function() {
+    $("#header-forFun").click(function () {
 
         let content = "content-forFun";
 
         hideContent(content);
     });
 
-    $( "#header-docs" ).click(function() {
+    $("#header-docs").click(function () {
 
         let content = "content-forFun";
 
         hideContent(content);
     });
- });
+});
 
 // CONTENT
 function hideContent(content) {
@@ -40,9 +40,16 @@ function hideContent(content) {
     let elements = document.getElementsByClassName("content");
 
     Array.prototype.forEach.call(elements, element => {
-        
+
         element.style.display = "none";
     });
 
-    $( "#"+content ).fadeIn(fadeInSpeed);
+    $("#" + content).fadeIn(fadeInSpeed);
+}
+
+// HOVER
+function hoverSkill() {
+
+    let element = document.getElementsByClassName(":focus")
+
 }
